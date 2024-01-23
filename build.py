c07dec7fe886f77b949eb4e45f4858bf6f40cf19
Rothos <hrothgarrrr@gmail.com>
2024-01-23 02:24:11 -0600

    minor updates to site; rebuild

--- build.py
+++ build.py
@@ -3,6 +3,8 @@ Build the static site which deploys to gar.lol/portraits via Github Pages.
 """
 
 import os
+from bs4 import BeautifulSoup as bs # for prettifying HTML
+from bs4 import formatter as bs_formatter # for prettifying HTML
 from natsort import natsorted
 from PIL import Image  # Import PIL to get image dimensions
 
@@ -62,36 +64,36 @@ def generate_html():
     bodyopen = "<body>\n"
     header = '<h1>Portraits</h1>\n'
     top_text = f"""<p>
-        As an ongoing art project, I sometimes ask people to draw me. This
-        project has undergone long periods of stagnation, but I haven't ever
-        given up on it. Currently, I have a total of {total_images} portraits
-        collected on this page.
-        </p>
-        <p>
-        I welcome any portraits you want to send me, and I will almost
-        definitely include them on this page. (I have omitted a total of two
-        portraits from this page ({n_ignored}/{total_images+n_ignored}, or
-        {"%1.1f" % (100*n_ignored/(total_images+n_ignored))}%), for different
-        reasons. Only in very special cases do I not include a portrait.)
-        </p>
-        <p>
-        For details about how I processed the images and created this gallery,
-        see the very bottom of the page.
-        </p>\n"""
+    As an ongoing art project, I sometimes ask people if they'd like to to
+    draw me. This project has undergone long periods of stagnation, but I
+    haven't ever given up on it. Currently, I have a total of {total_images}
+    portraits collected on this page.
+    </p>
+    <p>
+    I welcome any portraits you want to send me, and I will almost
+    definitely include them on this page. (I have omitted a total of two
+    portraits from this page ({n_ignored}/{total_images+n_ignored}, or
+    {"%1.1f" % (100*n_ignored/(total_images+n_ignored))}%), for different
+    reasons. Only in very special cases do I not include a portrait.)
+    </p>
+    <p>
+    For details about how I processed the images and created this gallery,
+    see the very bottom of the page.
+    </p>\n"""
     gallerytag = '<div id="gallery">\n'
     galleryendtag = '</div>\n'
     bottom_text = '<p>[details]</p>\n'
     bodyclose = """
-            <script type="module">
-            import PhotoSwipeLightbox from './js/photoswipe-lightbox.esm.min.js';
-            const lightbox = new PhotoSwipeLightbox({
-              gallery: '#gallery',
-              children: 'a',
-              pswpModule: () => import('./js/photoswipe.esm.min.js')
-            });
-            lightbox.init();
-            </script>
-        </body></html>"""
+    <script type="module">
+        import PhotoSwipeLightbox from './js/photoswipe-lightbox.esm.min.js';
+        const lightbox = new PhotoSwipeLightbox({
+            gallery: '#gallery',
+            children: 'a',
+            pswpModule: () => import('./js/photoswipe.esm.min.js')
+        });
+        lightbox.init();
+    </script>
+</body></html>"""
 
     html_content = head + bodyopen + header + top_text \
                  + gallerytag \
@@ -110,8 +112,12 @@ def main():
     html_content = generate_html()
     css_content = generate_css()
 
+    soup = bs(html_content, features="html.parser")
+    formatter = bs_formatter.HTMLFormatter(indent=2)
+    prettyHTML = soup.prettify(formatter=formatter)
+
     with open("index.html", "w") as html_file:
-        html_file.write(html_content)
+        html_file.write(prettyHTML)
 
     # with open("css/styles.css", "w") as css_file:
     #     css_file.write(css_content)
