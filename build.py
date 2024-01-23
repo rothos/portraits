be9ec058e168709e4e1320627f6c51f06717ee8b
Rothos <hrothgarrrr@gmail.com>
2024-01-23 02:10:53 -0600

    adding a lightbox viewer to the site; rebuild

--- build.py
+++ build.py
@@ -4,12 +4,18 @@ Build the static site which deploys to gar.lol/portraits via Github Pages.
 
 import os
 from natsort import natsorted
+from PIL import Image  # Import PIL to get image dimensions
 
 # Total image count
 total_images = 0
 
 # List of filenames to ignore
 ignore_list = ["book1_13.png", "book1_38.png"]
+
+# Function to get image dimensions
+def get_image_dimensions(image_path):
+    with Image.open(image_path) as img:
+        return img.size
 
 # Function to generate image gallery HTML code
 def generate_gallery(header, folder_name, gallery_id):
@@ -30,8 +36,11 @@ def generate_gallery(header, folder_name, gallery_id):
         image_path = os.path.join(folder_name, filename)
         thumbnail_path = os.path.join(folder_name, filename[:-4] + "t.png")
         
-        gallery_html += f'    <a href="{image_path}">\n'
-        gallery_html += f'        <img alt="{image_path}, a portrait drawing of me" src="{thumbnail_path}"/>\n'
+        # Get image dimensions
+        width, height = get_image_dimensions(image_path)
+
+        gallery_html += f'    <a href="{image_path}" data-pswp-width="{width}" data-pswp-height="{height}" target="_blank">\n'
+        gallery_html += f'        <img alt="portrait #{total_images}, a portrait drawing of me" src="{thumbnail_path}"/>\n'
         gallery_html += '    </a>\n'
 
     gallery_html += '</div>\n'
@@ -46,6 +55,11 @@ def generate_html():
     books_gallery = generate_gallery("Sketchbook portraits", "books", "books_gallery")
     misc_gallery = generate_gallery("Miscellaneous portraits", "misc", "misc_gallery")
 
+    head = """<!DOCTYPE html><html><head>
+            <title>Portraits</title>
+            <link rel="stylesheet" href="./css/photoswipe.css">
+        </head>\n"""
+    bodyopen = "<body>\n"
     header = '<h1>Portraits</h1>\n'
     top_text = f"""<p>
         As an ongoing art project, I sometimes ask people to draw me. This
@@ -64,9 +78,26 @@ def generate_html():
         For details about how I processed the images and created this gallery,
         see the very bottom of the page.
         </p>\n"""
+    gallerytag = '<div id="gallery">\n'
+    galleryendtag = '</div>\n'
     bottom_text = '<p>[details]</p>\n'
+    bodyclose = """
+            <script type="module">
+            import PhotoSwipeLightbox from './js/photoswipe-lightbox.esm.min.js';
+            const lightbox = new PhotoSwipeLightbox({
+              gallery: '#gallery',
+              children: 'a',
+              pswpModule: () => import('./js/photoswipe.esm.min.js')
+            });
+            lightbox.init();
+            </script>
+        </body></html>"""
 
-    html_content = header + top_text + books_gallery + misc_gallery + bottom_text
+    html_content = head + bodyopen + header + top_text \
+                 + gallerytag \
+                 + books_gallery + misc_gallery \
+                 + galleryendtag \
+                 + bottom_text + bodyclose
     return html_content
 
 # Function to generate CSS styling
@@ -82,8 +113,8 @@ def main():
     with open("index.html", "w") as html_file:
         html_file.write(html_content)
 
-    with open("styles.css", "w") as css_file:
-        css_file.write(css_content)
+    # with open("css/styles.css", "w") as css_file:
+    #     css_file.write(css_content)
 
 if __name__ == "__main__":
     main()
